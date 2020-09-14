e91cf384594e3d9a92cdae1a6dcd02c6fc7586f5
vladsemyr <vladsemyr585@gmail.com>
2020-09-14 10:48:00 +0300

    Производится поиск статей по годам (однопоточно)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from elsapy.elsprofile import ElsAuthor, ElsAffil
 from elsapy.elsdoc import FullDoc, AbsDoc
 from elsapy.elssearch import ElsSearch
 import json
+
+from threading import Thread
     
 ## Load configuration
 con_file = open("config.json")
@@ -15,9 +17,81 @@ con_file.close()
 client = ElsClient(config['apikey'])
 client.inst_token = config['insttoken']
 
-## Initialize doc search object using ScienceDirect and execute search, 
-#   retrieving all results
+class Searcher(Thread):
+    def __init__(self, search_string, filename, title, year):
+        self._search_string = search_string
+        self._filename = filename
+        self._title = title
+        self._year = year
+    
+    def run(self):
+        doc_srch = ElsSearch(self._search_string,'sciencedirect')
+        doc_srch.execute(client, get_all = False)
+        
+        with open(self._filename, "a") as f:
+            f.write(f"{self._title};{self._year};{doc_srch.tot_num_res}\n")
+        pass
 
-doc_srch = ElsSearch("DSP AND PUBYEAR = 2020",'sciencedirect')
-doc_srch.execute(client, get_all = False)
-print ("doc_srch has", doc_srch.tot_num_res, "results.")
+class Article:
+    _begin_year = 2000
+    _end_year = 2020
+    _filename = "out.csv"
+    _similar = []
+    
+    @classmethod
+    def default_init(cls, d):
+        Article._begin_year = d["begin_year"]
+        Article._end_year = d["end_year"]
+        Article._filename = d["output_filename"]
+    
+    def __init__(self, title: str, d: dict):
+        self._title = title
+            
+        if "begin_year" in d:
+            self._begin_year = d["begin_year"]
+        
+        if "end_year" in d:
+            self._end_year = d["end_year"]
+        
+        if "output_filename" in d:
+            self._filename = d["output_filename"]
+        
+        if "similar" in d:
+            self._similar = d["similar"]
+    
+    def search(self):
+        search_string_without_year = f"({self._title})" + "".join(list(f" OR ({x})" for x in self._similar))
+        
+        for year in range(self._begin_year, self._end_year + 1):
+            search_string_year = f" AND PUBYEAR = {year}"
+            
+            s = Searcher(search_string_without_year + search_string_year, self._filename, self._title, year)
+            s.run()
+
+    
+    def clean_file(self):
+        with open(self._filename, "w") as _:
+            pass
+    
+    @property
+    def filename(self):
+        return self._filename
+
+
+with open("search.json", "r") as f:
+    search_obj = json.load(f)
+
+Article.default_init(search_obj["default"])
+articles = list(Article(key, val) for (key,val) in search_obj["titles"].items())
+
+for a in articles:
+    a.clean_file()
+
+for a in articles:
+    a.search()
+
+
+
+
+# https://www.sciencedirect.com/search?qs=DSP%20processor%20asd
+# https://dev.elsevier.com/sc_search_tips.html
